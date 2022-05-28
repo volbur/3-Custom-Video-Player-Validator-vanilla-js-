555936ed2a91deb7ff9e7cdd5fc748ad948d44c7
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 12:15:33 +0300

    [script.js] created function for togle video status

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-'use strict';
+"use strict";
 
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
