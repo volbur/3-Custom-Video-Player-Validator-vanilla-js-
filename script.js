d1334f6c44bba878de1e5337e0ccc0a7a434b946
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 12:34:13 +0300

    [script.js] event listeners for btns and progress

--- script.js
+++ script.js
@@ -59,3 +59,9 @@ video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
