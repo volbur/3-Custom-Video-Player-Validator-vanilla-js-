7354a330a966d5ba4cec8108bbceba976fcce990
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 12:11:32 +0300

    [script.js] addid refs

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
