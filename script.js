526b29caa17de79935f10c5529d7caafcd65016e
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 12:18:42 +0300

    [script.js] created function for update play/pause icon

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function toggleVideoStatus() {
     video.pause();
   }
 }
+
+// update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
